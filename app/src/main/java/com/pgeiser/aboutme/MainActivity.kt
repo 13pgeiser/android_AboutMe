39bda9f3eb899b11e9860ecf95f98be2dd2349ea
13pgeiser <pgeiser@pgeiser.com>
2020-12-09 11:55:53 +0100

    Use databinding everywhere.

--- app/src/main/java/com/pgeiser/aboutme/MainActivity.kt
+++ app/src/main/java/com/pgeiser/aboutme/MainActivity.kt
@@ -5,22 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.pgeiser.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-
+    private val myName: MyName = MyName("P. Geiser")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
-        // findViewById<Button>(R.id.done_button).setOnClickListener {
-        //     addNickName(it)
-        // }
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
         binding.doneButton.setOnClickListener {
             addNickName(it)
         }
@@ -28,10 +22,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickName(view: View) {
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
+            invalidateAll()
         }
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
